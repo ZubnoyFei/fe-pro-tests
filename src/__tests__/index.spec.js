d1dacd3cb099ea15cd36edaa2e13bffe9d977e65
Your Name <ustinovoleksij@gmail.com>
2022-04-28 09:50:04 +0300

    Added tests to the index.spec.js

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -3,15 +3,52 @@ import {
   sumOfArray,
   divisibleBy,
   addDotsToString,
-  objectEntries,
+  objectEntries
 } from '../index';
 
-describe('changeStringCase()', () => {});
+describe('changeStringCase()', () => {
+  it('Changes the string to uppercase or lowercase', () => {
+    const result = changeStringCase('THIS IS THE UPPERCASE STRING');
+    expect(result).toEqual(
+      'this is the uppercase string'
+    );
+  });
+  describe('changeStringCase()', () => {
+    it('Changes the string to uppercase or lowercase', () => {
+      const result = changeStringCase('this is the lowercase string');
+      expect(result).toEqual('THIS IS THE LOWERCASE STRING');
+    });
+  });
+});
 
-describe('sumOfArray()', () => {});
+describe('sumOfArray()', () => {
+  it('Returns the sum of numbers', () => {
+    const result = sumOfArray([1, 2, 3, 4, 5]);
+    expect(result).toEqual([15]);
+  });
+});
 
-describe('divisibleBy()', () => {});
+describe('divisibleBy()', () => {
+  it('Returns the even numbers', () => {
+    const result = divisibleBy([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
+    expect(result).toEqual([2, 4, 6, 8, 10]);
+  });
+});
 
-describe('addDotsToString()', () => {});
+describe('addDotsToString()', () => {
+  it('Adds dots to sting if the string is too big', () => {
+    const string = addDotsToString('This is the expected string', 3);
+    expect(string).toEqual('This is the');
+  });
+});
 
-describe('objectEntries()', () => {});
+describe('objectEntries()', () => {
+  it('Iterates over an object', () => {
+    const objectEntr = objectEntries({
+      name: 'Oleksii',
+      lastName: 'Ustinov',
+      age: 30,
+    });
+    expect(objectEntr).toEqual(["Oleksii", "Ustinov", 30]);
+  });
+});
